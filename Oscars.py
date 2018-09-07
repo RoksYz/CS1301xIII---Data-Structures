b6083799a4d546586a3e7d0add7e50c00554f0ac
Pauloroks <pauloroks@gmail.com>
2018-09-07 18:06:05 -0300

    Update Oscars.py

--- Oscars.py
+++ Oscars.py
@@ -1,13 +1,4 @@
-#Write a function called most_oscars, which takes in one
-#parameter, a dictionary. This dictionary maps names to the
-#number of Academy Awards for which they have been nominated.
-#This function should return a tuple containing the name and
-#number of nominations for the person who has the most
-#nominations.
-#
-#You may assume there will not be a tie for the actor with
-#the most nominations (although there may be other ties in
-#the list).
+
 
 def most_oscars(Adict):
     value = 0
